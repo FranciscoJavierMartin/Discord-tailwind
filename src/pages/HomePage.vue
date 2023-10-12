6f28e18e57e4c7783de12c959691bbf35a22e798
Francisco <javiermartincuenca@gmail.com>
2023-10-12 13:38:23 +0200

    Change colors

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="flex text-white h-screen">
-    <div class="bg-gray-800 p-3 space-y-2 overflow-y-scroll">
+  <div class="flex h-screen text-gray-100">
+    <div class="space-y-2 overflow-y-scroll bg-gray-900 p-3">
       <div
         v-for="(_, index) in Array(40)"
         :key="index"
-        class="bg-white text-gray-800 w-12 h-12 rounded-full flex items-center justify-center"
+        class="flex w-12 h-12 items-center justify-center rounded-full bg-white text-gray-800"
       >
         {{ index }}
       </div>
     </div>
-    <div class="bg-gray-700 w-60 flex flex-col">
-      <div class="p-3 h-12 shadow-md flex items-center flex-shrink-0">
-        Tailwind CSS
-      </div>
-      <div class="p-3 flex-1 overflow-y-scroll space-y-2">
+    <div class="flex w-60 flex-col bg-gray-800">
+      <div class="flex h-12 items-center px-3 shadow-md">Tailwind CSS</div>
+      <div class="flex-1 space-y-2 overflow-y-scroll p-3 text-gray-300">
+        <p class="text-white">Channel (unread)</p>
+        <p class="text-white">Channel (unread)</p>
         <p v-for="(_, index) in Array(40)" :key="index">Channel {{ index }}</p>
       </div>
     </div>
-    <div class="bg-gray-600 flex-1 flex flex-col">
-      <div class="p-3 h-12 flex items-center shadow-md">General</div>
-      <div class="p-3 flex-1 space-y-4 overflow-y-scroll">
+    <div class="flex flex-1 flex-col bg-gray-700">
+      <div class="flex h-12 items-center px-3 shadow-md">General</div>
+      <div class="flex-1 space-y-4 overflow-y-scroll p-3">
         <p v-for="(_, index) in Array(40)" :key="index">
           Message {{ index }}. Lorem ipsum dolor sit amet consectetur
           adipisicing elit. Sequi a voluptatem dolores aliquam sed, unde atque
