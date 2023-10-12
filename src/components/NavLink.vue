051704179fe2a916d12a24af50b962b18bdced9a
Francisco <javiermartincuenca@gmail.com>
2023-10-12 18:36:10 +0200

    Navbar component

---
+++ src/components/NavLink.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="relative block group">
+    <div class="absolute flex items-center h-full -left-3">
+      <div
+        class="w-1 bg-white rounded-r origin-left duration-200 transition-all"
+        :class="{
+          'h-5 scale-0 opacity-0 group-hover:opacity-100 group-hover:scale-100':
+            route.path !== to,
+          'h-10': route.path === to,
+        }"
+      />
+    </div>
+    <div class="group-active:translate-y-px">
+      <RouterLink
+        :to="to"
+        class="flex items-center justify-center w-12 h-12 transition-all duration-200 group-hover:bg-brand group-hover:rounded-2xl group-hover:text-white"
+        :class="{
+          'bg-brand rounded-2xl text-white': route.path === to,
+          'bg-gray-700 rounded-3xl text-gray-100': route.path !== to,
+        }"
+      >
+        <slot />
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+import { useRoute } from 'vue-router';
+
+defineProps<{ to: string }>();
+
+const route = useRoute();
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,34 +1,10 @@
 <template>
   <div class="flex h-screen text-gray-100">
     <div class="p-3 space-y-2 overflow-y-scroll bg-gray-900">
-      <div class="relative block group">
-        <div class="absolute flex items-center h-full -left-3">
-          <div
-            class="w-1 bg-white rounded-r origin-left duration-200 transition-all"
-            :class="{
-              'h-5 scale-0 opacity-0 group-hover:opacity-100 group-hover:scale-100':
-                route.path !== '/',
-              'h-10': route.path === '/',
-            }"
-          />
-        </div>
-        <div class="group-active:translate-y-px">
-          <RouterLink
-            to="/"
-            class="flex items-center justify-center w-12 h-12 transition-all duration-200 group-hover:bg-brand rounded-3xl bg-gray-700 text-gray-100 group-hover:rounded-2xl group-hover:text-white"
-            active-class="bg-brand rounded-2xl text-white"
-          >
-            <DiscordIcon class="h-5 w-7" />
-          </RouterLink>
-        </div>
-      </div>
-      <RouterLink
-        :to="`/servers/1`"
-        class="flex items-center justify-center w-12 h-12 transition-all duration-200 hover:bg-brand rounded-3xl bg-gray-700 text-gray-100 hover:rounded-2xl hover:text-white"
-        active-class="bg-brand rounded-2xl text-white"
-      >
-        S1
-      </RouterLink>
+      <NavLink to="/">
+        <DiscordIcon class="h-5 w-7" />
+      </NavLink>
+      <NavLink :to="`/servers/1`"> S1 </NavLink>
     </div>
     <slot />
   </div>
@@ -36,7 +12,5 @@
 
 <script lang="ts" setup>
 import DiscordIcon from '@/components/DiscordIcon.vue';
-import { useRoute } from 'vue-router';
-
-const route = useRoute();
+import NavLink from '@/components/NavLink.vue';
 </script>
