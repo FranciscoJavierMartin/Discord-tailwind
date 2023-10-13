a56737501ad3ad47bb35018e732a6b01c4fdde12
Francisco <javiermartincuenca@gmail.com>
2023-10-13 11:53:31 +0200

    Style unread channel

--- src/components/ChannelLink.vue
+++ src/components/ChannelLink.vue
@@ -1,14 +1,19 @@
 <template>
   <RouterLink
     :to="`/servers/${$route.params.sid}/channels/${id}`"
-    class="flex items-center text-gray-300 px-2 mx-2 py-1 rounded group hover:text-gray-100 group hover:bg-gray-550/[0.16]"
+    class="flex items-center text-gray-300 px-2 mx-2 py-1 rounded group hover:text-gray-100 group relative hover:bg-gray-550/[0.16]"
     :class="{
       'text-white bg-gray-550/[0.32]': +$route.params.cid === +id,
-      'text-white hover:bg-gray-550/[0.16]': unread,
-      'text-gray-300 hover:text-gray-100 hover:bg-gray-550/[0.16]': !unread,
+      'text-white hover:bg-gray-550/[0.16] active:bg-gray-550/[0.24]': unread,
+      'text-gray-300 hover:text-gray-100 hover:bg-gray-550/[0.16] active:bg-gray-550/[0.24]':
+        !unread,
     }"
     active-class="bg-gray-550/[0.32] text-white"
   >
+    <div
+      v-if="+$route.params.cid !== +id && unread"
+      class="absolute w-2 h-2 bg-white left-0 -ml-3 rounded-r-full"
+    />
     <component :is="Icon" class="w-5 h-5 mr-1.5 text-gray-400" />
     {{ label }}
     <AddPersonIcon
